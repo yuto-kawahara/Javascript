c74e56d0765168117304ed7740bcdbf4b7bdfb05
yuto-kawahara <mashroom1384@gmail.com>
2021-09-12 23:08:07 +0900

    ・v-onディレクティブ ・イベント修飾子 ・キー修飾子 ・v-onディレクティブの省略記法

--- Vue.js/main.js
+++ Vue.js/main.js
@@ -12,7 +12,9 @@
       href:"https://twitter.com",
       id: 31
     },
-    count: 0
+    count: 0,
+    x:0,
+    y:0
   },
   // Vueインスタンスが保持しているメソッド
   methods: {
@@ -20,8 +22,27 @@
       this.message = "Hello Vue.js"
       return "Hi";
     },
-    countUp() {
-      this.count += 1
+    countUp(times) {
+      this.count += 1 * times;
+    },
+    // 引数にイベントオブジェクトを取得することができる
+    changeMousePosition(divNum,event)
+    {
+      this.x = event.clientX / divNum;
+      this.y = event.clientY / divNum;
+    },
+    noEvent(event)
+    {
+      //イベントを伝搬させないメソッド
+      event.stopPropagation();
+    },
+    noEventPrev(event)
+    {
+      event.preventDefault();
+    },
+    myAlert()
+    {
+      alert("アラートです");
     }
   }
 })
