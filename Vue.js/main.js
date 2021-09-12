a54d7b0944dd8d3f38f1f6a9cd5689a5b87ca3e3
yuto-kawahara <mashroom1384@gmail.com>
2021-09-12 19:54:07 +0900

    Vue.js基礎

---
+++ Vue.js/main.js
@@ -0,0 +1,27 @@
+﻿new Vue({
+  el: '#app',
+  // Vueインスタンスが保持しているデータ
+  data: {
+    url: "https://google.com",
+    urlTwitter: "https://twitter.com",
+    number: 31,
+    attribute: "href",
+    html: "<h1>h1です<h1>",
+    message: "HelloWorld!",
+    twitterObject: {
+      href:"https://twitter.com",
+      id: 31
+    },
+    count: 0
+  },
+  // Vueインスタンスが保持しているメソッド
+  methods: {
+    sayHi(){
+      this.message = "Hello Vue.js"
+      return "Hi";
+    },
+    countUp() {
+      this.count += 1
+    }
+  }
+})
