e0950abb8454d6efe892f07b1572189e4f66b5fa
yuto-kawahara <mashroom1384@gmail.com>
2021-09-13 11:01:09 +0900

    条件付きレンダリングとリストレンダリング

---
+++ Vue.js/条件付きレンダリングとリストレンダリング/main.js
@@ -0,0 +1,23 @@
+﻿new Vue({
+  el: '#app',
+  // Vueインスタンスが保持しているデータ
+  // dataはあくまでも初期値を設定する場所なので、動的な表現はできない
+  data: {
+    ok: false,
+    maybeOK: false,
+    fruits: ["りんご","バナナ","ぶどう"],
+    object: {
+      firstName: "タロウ",
+      lastName: "田中",
+      age: 21
+    }
+  },
+  // computed内で関数を記述しreturnによって値を得ることができる(あくまでもプロパティのためreturnが必要)
+  computed: {
+  },
+  methods: {
+    remove() {
+      this.fruits.shift();
+    }
+  },
+})
