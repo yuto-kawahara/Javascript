cf606faca5a6e890cd79743767c3a3443b71147e
yuto-kawahara <mashroom1384@gmail.com>
2021-10-11 16:35:19 +0900

    スライダーアニメーション

---
+++ スライダーアニメーション/main.js
@@ -0,0 +1,38 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+  new HeroSlider();
+});
+
+
+class HeroSlider {
+  constructor() {
+    this.swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 1,
+      speed: 1000,
+      breakpoints: {
+        1024: {
+          slidesPerView: 2,
+        }
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
+  }
+}
