ebb47da78f3fb56c2035d4c20ecc323fefd05847
yuto-kawahara <mashroom1384@gmail.com>
2022-01-18 13:11:50 +0900

    [Add]非同期処理

---
+++ Udemy/非同期処理/非同期処理のチェーン/main.js
@@ -0,0 +1,18 @@
+function sleep(callback, val) {
+  setTimeout(function () {
+    console.log(val++);
+    callback(val++);
+  }, 1000);
+}
+
+sleep(function (val) {
+  sleep(function (val) {
+    sleep(function (val) {
+      sleep(function (val) {
+        sleep(function (val) {
+          sleep(function (val) {}, val);
+        }, val);
+      }, val);
+    }, val);
+  }, val);
+}, 0);
